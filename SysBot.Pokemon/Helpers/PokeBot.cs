3b23547234e4c7afc73a868a394a7d15edea13cc
Gengar <80122551+bdawg1989@users.noreply.github.com>
2025-06-13 19:13:24 -0500

    Support Wolfe's Incineroar, Add latest ALM fixes

--- SysBot.Pokemon/Helpers/PokeBot.cs
+++ SysBot.Pokemon/Helpers/PokeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v1.1.0";
+        public const string Version = "v1.1.1";
     }
 }
