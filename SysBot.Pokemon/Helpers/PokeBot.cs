1fab215a105c3ada4886aa79bd99294f6347c7c6
Gengar <80122551+bdawg1989@users.noreply.github.com>
2025-07-22 19:28:42 -0500

    Add latest outbreak support (capsakid, petilil, oddish)

--- SysBot.Pokemon/Helpers/PokeBot.cs
+++ SysBot.Pokemon/Helpers/PokeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v1.1.7";
+        public const string Version = "v1.1.8";
     }
 }
