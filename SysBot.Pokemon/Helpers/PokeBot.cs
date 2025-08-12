b9d896fc948a0d13b3ddc7dc097710193811ede6
bdawg1989 <80122551+hexbyt3@users.noreply.github.com>
2025-08-12 09:31:19 -0500

    Latest Core/ALM changes

--- SysBot.Pokemon/Helpers/PokeBot.cs
+++ SysBot.Pokemon/Helpers/PokeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v1.1.14a";
+        public const string Version = "v1.1.14b";
     }
 }
