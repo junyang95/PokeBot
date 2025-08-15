5dad5e367d6b8f3d9408e3719d521bb94e634baa
hexbyt3 <80122551+hexbyt3@users.noreply.github.com>
2025-08-15 18:15:55 -0500

    Update deps

--- SysBot.Pokemon/Helpers/PokeBot.cs
+++ SysBot.Pokemon/Helpers/PokeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v1.1.14d";
+        public const string Version = "v1.1.14e";
     }
 }
