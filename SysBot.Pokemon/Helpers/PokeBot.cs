d20474b10277b23bc0be7744193920a2d8d9d948
hexbyt3 <80122551+hexbyt3@users.noreply.github.com>
2025-08-21 20:23:43 -0500

    Shiny Chien-Pao support

--- SysBot.Pokemon/Helpers/PokeBot.cs
+++ SysBot.Pokemon/Helpers/PokeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v1.1.14e";
+        public const string Version = "v1.1.15";
     }
 }
