894bc2c87f799d3e1636743ff5b66767a7953133
bdawg1989 <80122551+hexbyt3@users.noreply.github.com>
2025-09-25 17:09:44 -0500

    Update for Shiny Koraidon/Miraidon

--- SysBot.Pokemon/Helpers/PokeBot.cs
+++ SysBot.Pokemon/Helpers/PokeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v1.1.20";
+        public const string Version = "v1.1.21";
     }
 }
