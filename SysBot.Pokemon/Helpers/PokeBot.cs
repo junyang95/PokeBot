d3a32e0e3d67c63fc3c563b67cf4697ea09aa45e
bdawg1989 <80122551+hexbyt3@users.noreply.github.com>
2025-09-27 13:07:46 -0500

    Update ALM

--- SysBot.Pokemon/Helpers/PokeBot.cs
+++ SysBot.Pokemon/Helpers/PokeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v1.1.22";
+        public const string Version = "v1.1.22a";
     }
 }
