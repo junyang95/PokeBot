1a37f89984dab50f706f22e9fa0a7ada0a18e339
bdawg1989 <80122551+hexbyt3@users.noreply.github.com>
2025-09-29 10:14:02 -0500

    Version Bump

--- SysBot.Pokemon/Helpers/PokeBot.cs
+++ SysBot.Pokemon/Helpers/PokeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v1.1.22b";
+        public const string Version = "v1.1.22c";
     }
 }
