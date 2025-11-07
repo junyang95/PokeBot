c7240eda78d8f03c4c91ab3db2701a095e4050a0
hexbyt3 <hexbyt3@users.noreply.github.com>
2025-11-07 12:55:35 -0600

    Update to latest PKHeX 11.7.25

--- SysBot.Pokemon/Helpers/PokeBot.cs
+++ SysBot.Pokemon/Helpers/PokeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v1.2.4a";
+        public const string Version = "v1.2.5";
     }
 }
