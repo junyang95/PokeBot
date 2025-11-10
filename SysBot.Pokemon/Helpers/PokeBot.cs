3f55c85ab36acab273747e248533e0997471559d
hexbyt3 <hexbyt3@users.noreply.github.com>
2025-11-10 06:23:48 -0600

    Bump version to v1.2.6d

--- SysBot.Pokemon/Helpers/PokeBot.cs
+++ SysBot.Pokemon/Helpers/PokeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v1.2.6c";
+        public const string Version = "v1.2.6d";
     }
 }
