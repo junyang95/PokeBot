81e69a3e95dcdb6b5982433d948a54620ba68973
hexbyt3 <hexbyt3@users.noreply.github.com>
2025-11-15 11:00:01 -0600

    Update ALM/PKHeX to latest ZA legality commits

--- SysBot.Pokemon/Helpers/PokeBot.cs
+++ SysBot.Pokemon/Helpers/PokeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v1.2.6f";
+        public const string Version = "v1.2.6g";
     }
 }
