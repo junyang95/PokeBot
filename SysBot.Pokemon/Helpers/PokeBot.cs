ed4b008c1dcf59266f37f8ed11ea57cc8039a0ff
hexbyt3 <hexbyt3@users.noreply.github.com>
2025-11-22 19:30:14 -0600

    Version bump

--- SysBot.Pokemon/Helpers/PokeBot.cs
+++ SysBot.Pokemon/Helpers/PokeBot.cs
@@ -6,6 +6,6 @@ namespace SysBot.Pokemon.Helpers
 
         public const string ConfigPath = "config.json";
 
-        public const string Version = "v1.3.0";
+        public const string Version = "v1.3.1";
     }
 }
